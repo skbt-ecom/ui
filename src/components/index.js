f29a00ba7cd0d89cf039e5186a8eff2f8806a715
Ramil <ayupovri@sovcombank.ru>
2019-06-18 16:20:32 +0300

    added Readme.md && scripts section improvements

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,7 @@
-export { default as Button } from './Button';
-export { default as Switch } from './Switch';
+// Theme export
 export { default as theme } from '../style/theme';
 export { ThemeProvider } from '@material-ui/styles';
+
+// Components export
+export { default as Button } from './Button';
+export { default as Switch } from './Switch';
