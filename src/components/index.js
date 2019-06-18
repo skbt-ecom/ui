b27ca62042d646a0c63b735314609b90d23848c5
Ramil <ayupovri@sovcombank.ru>
2019-06-18 11:40:28 +0300

    added alpha theme support 2

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,4 @@
-import { ThemeProvider } from '@material-ui/styles';
-
 export { default as Button } from './Button';
 export { default as Switch } from './Switch';
-// export ThemeProvider
+export { default as theme } from '../style/theme';
+export { ThemeProvider } from '@material-ui/styles';
