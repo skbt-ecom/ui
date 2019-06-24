015d0af43c52c7ccffb5613b68daa2df57e99ba9
Ramil <ayupovri@sovcombank.ru>
2019-06-24 17:23:14 +0300

    Button component added

--- src/components/Button/stories.js
+++ src/components/Button/stories.js
@@ -5,8 +5,20 @@ import { action } from '@storybook/addon-actions';
 // Import our component from this folder
 import Button from './Button';
 
-storiesOf('Button', module).add('Default', () => (
-  <Button color="primary" variant="contained">
-    Lol
-  </Button>
-));
+import { muiTheme } from 'storybook-addon-material-ui';
+import theme from '../../style/theme';
+
+storiesOf('Button', module)
+  .addDecorator(muiTheme([theme]))
+  .add('Default', () => (
+    <>
+      <Button color="primary" onClick={action('clicked btn1')}>
+        Button
+      </Button>
+      <br />
+      <br />
+      <Button color="secondary" onClick={action('clicked btn2')}>
+        Button
+      </Button>
+    </>
+  ));
