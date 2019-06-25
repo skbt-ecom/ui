7021bb99a3c6160966b3fcad47281f8e938b83ca
Ramil <ayupovri@sovcombank.ru>
2019-06-25 13:01:31 +0300

    Google Font link added to <head> of storybook

--- src/style/theme.js
+++ src/style/theme.js
@@ -1,25 +1,6 @@
 import { createMuiTheme } from '@material-ui/core/styles';
 
 const theme = createMuiTheme({
-  // typography: {
-  //   fontFamily: [
-  //     'Bebas',
-  //     '-apple-system',
-  //     'BlinkMacSystemFont',
-  //     '"Segoe UI"',
-  //     'Roboto',
-  //     '"Helvetica Neue"',
-  //     'Arial',
-  //     'sans-serif',
-  //     '"Apple Color Emoji"',
-  //     '"Segoe UI Emoji"',
-  //     '"Segoe UI Symbol"',
-  //   ].join(','),
-  // },
-  // typography: {
-  //   // fontFamily: ['Roboto', 'Helvetica', 'Arial', 'sans - serif'].join(','),
-  //   fontFamily: ['Bebas', 'Helvetica', 'Arial', 'sans-serif'].join(','),
-  // },
   palette: {
     primary: { main: '#003791' },
     secondary: { main: '#FF4B5F' },
