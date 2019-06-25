cc90d57ccb8074b54273b7358de1cf228dc16a85
Ramil <ayupovri@sovcombank.ru>
2019-06-25 12:08:16 +0300

    test fonts

--- src/components/Button/stories.js
+++ src/components/Button/stories.js
@@ -4,6 +4,8 @@ import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 // Import our component from this folder
 import Button from './Button';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
 
 import { muiTheme } from 'storybook-addon-material-ui';
 import theme from '../../style/theme';
@@ -12,9 +14,15 @@ storiesOf('Button', module)
   .addDecorator(muiTheme([theme]))
   .add('Default', () => (
     <>
-      <Button color="primary" onClick={action('clicked btn1')}>
-        Button
-      </Button>
+      <Typography component="div">
+        <Button
+          color="primary"
+          onClick={action('clicked btn1')}
+          fontWeight={700}
+        >
+          Button
+        </Button>
+      </Typography>
       <br />
       <br />
       <Button color="secondary" onClick={action('clicked btn2')}>
