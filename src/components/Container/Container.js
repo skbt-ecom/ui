ea91a33a884a4880769d490c8e339988099234a7
Tahir Nurmiev <nurmievtk@sovcombank.ru>
2019-07-10 17:01:03 +0300

    Container: max-width 425

--- src/components/Container/Container.js
+++ src/components/Container/Container.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import Container from '@material-ui/core/Container';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    [theme.breakpoints.down('xs')]: {
+      maxWidth: 425,
+    },
+  },
+}));
 
 const ContainerComponent = React.memo(props => {
-  return <Container fixed {...props} />;
+  const classes = useStyles();
+
+  return <Container classes={{ fixed: classes.root }} fixed {...props} />;
 });
 
 export default ContainerComponent;
