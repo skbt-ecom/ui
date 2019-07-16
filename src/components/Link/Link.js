3f657013c3ffd618f75230a792e763782a5fc559
Ramil <ayupovri@sovcombank.ru>
2019-07-16 12:16:45 +0300

    fix: Link styles support

--- src/components/Link/Link.js
+++ src/components/Link/Link.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import Link from '@material-ui/core/Link';
 
+import useStyles from './styles';
+
 const LinkComponent = React.memo(props => {
-  return <Link {...props} />;
+  const classes = useStyles(props);
+
+  return (
+    <Link
+      {...props}
+      classes={{
+        root: classes.root,
+        button: classes.button,
+      }}
+    />
+  );
 });
 
 export default LinkComponent;
---
+++ src/components/Link/styles.js
@@ -0,0 +1,8 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+  root: {},
+  button: {},
+});
+
+export default useStyles;
