ef65e92f946816a895e41f3ba51b406d51640a66
Ramil <ayupovri@sovcombank.ru>
2019-08-05 13:09:15 +0300

    fix: VeridyCode component allows only number

--- src/components/VerifyCode/VerifyCode.js
+++ src/components/VerifyCode/VerifyCode.js
@@ -4,11 +4,19 @@ import useStyles from './styles';
 const VerifyCode = React.memo(props => {
   const classes = useStyles(props);
 
+  const handleKeyDown = e => {
+    const iKeyCode = e.which ? e.which : e.keyCode;
+    if (iKeyCode != 46 && iKeyCode > 31 && (iKeyCode < 48 || iKeyCode > 57)) {
+      e.preventDefault();
+    }
+  };
+
   return (
     <div className={classes.verifyCodeWrapper}>
       <div className={classes.verifyCodeContainer}>
         <input
           className={classes.verifyCodeInput}
+          onKeyDown={handleKeyDown}
           maxLength="4"
           pattern="\d*"
           onChange={props.onChange}
