2f8c933a2d497096e2c5cb6ac77bd50a12b5fe1a
Ramil <ayupovri@sovcombank.ru>
2019-09-30 15:15:13 +0300

    fix: button styles

--- src/components/Button/styles.js
+++ src/components/Button/styles.js
@@ -2,19 +2,13 @@ import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles({
   root: {
-    fontSize: '20px',
+    paddingTop: '11px',
+    paddingBottom: '11px',
     fontFamily: '"Bebas", "Roboto", "Helvetica", "Arial", sans-serif',
     letterSpacing: '1px',
-    paddingTop: 0,
-    paddingBottom: 0,
-    boxShadow: 'none',
-    alignItems: 'baseline',
-    lineHeight: 59,
-    height: 59,
+    fontSize: '20px',
   },
-  label: {
-    lineHeight: 'normal',
-  },
+  label: {},
 });
 
 export default useStyles;
