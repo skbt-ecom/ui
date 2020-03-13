ce02dabcdf22b2103e5eb7995d5d151f2ec15a91
Ramil <ayupovri@sovcombank.ru>
2020-03-13 22:48:48 +0300

    AutocompleteField fixes

--- src/components/AutocompleteField/AutocompleteField.js
+++ src/components/AutocompleteField/AutocompleteField.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import TextField from '@material-ui/core/TextField';
 import Autocomplete from '@material-ui/lab/Autocomplete';
-import FormControl from '@material-ui/core/FormControl';
-import FormHelperText from '@material-ui/core/FormHelperText';
 
 const AutocompleteField = ({
   label,
@@ -12,21 +10,22 @@ const AutocompleteField = ({
   ...props
 }) => {
   return (
-    <FormControl
-      style={{ width: fullWidth ? '100%' : '' }}
-      error
-      variant="outlined"
-    >
-      <Autocomplete
-        forcePopupIcon={false}
-        getOptionLabel={option => option.label}
-        renderInput={params => (
-          <TextField {...params} label={label} variant="outlined" />
-        )}
-        {...props}
-      />
-      {helperText && <FormHelperText>{helperText}</FormHelperText>}
-    </FormControl>
+    <Autocomplete
+      forcePopupIcon={false}
+      getOptionLabel={option => option.label}
+      noOptionsText={'Нет вариантов'}
+      renderInput={params => (
+        <TextField
+          {...params}
+          label={label}
+          variant="outlined"
+          error={error}
+          helperText={helperText}
+          fullWidth
+        />
+      )}
+      {...props}
+    />
   );
 };
 
