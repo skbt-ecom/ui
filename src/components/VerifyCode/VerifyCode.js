d49a80ff4256305d562645b8088bdc0ee99c2b2d
Ramil <ayupovri@sovcombank.ru>
2020-03-17 10:32:29 +0300

    ref: VerifyCode component

--- src/components/VerifyCode/VerifyCode.js
+++ src/components/VerifyCode/VerifyCode.js
@@ -1,56 +1,69 @@
 import React from 'react';
+import FormControl from '@material-ui/core/FormControl';
+import FormHelperText from '@material-ui/core/FormHelperText';
+
 import useStyles from './styles';
+
+const acceptedKeys = [
+  // Backspace and Tab and Enter
+  8,
+  9,
+  13,
+  // Home and End
+  35,
+  36,
+  // left and right arrows
+  37,
+  39,
+  // Ins and Del
+  45,
+  46,
+];
 
 const VerifyCode = React.memo(props => {
   const classes = useStyles(props);
+  const { onChange, value, error, helperText } = props;
 
   const handleKeyDown = e => {
     const key = e.which || e.keyCode;
     if (
-      (!e.shiftKey &&
-        !e.altKey &&
-        !e.ctrlKey &&
-        // numbers
-        key >= 48 &&
-        key <= 57) ||
-      // Numeric keypad
-      (key >= 96 && key <= 105) ||
-      // Backspace and Tab and Enter
-      key == 8 ||
-      key == 9 ||
-      key == 13 ||
-      // Home and End
-      key == 35 ||
-      key == 36 ||
-      // left and right arrows
-      key == 37 ||
-      key == 39 ||
-      // Del and Ins
-      key == 46 ||
-      key == 45
+      !(
+        (!e.shiftKey &&
+          !e.altKey &&
+          !e.ctrlKey &&
+          // numbers
+          key >= 48 &&
+          key <= 57) ||
+        // Numeric keypad
+        (key >= 96 && key <= 105) ||
+        acceptedKeys.includes(key)
+      )
     ) {
-      // input is VALID
-    } else {
-      // input is INVALID
       e.preventDefault();
     }
   };
 
   return (
-    <div className={classes.verifyCodeWrapper}>
-      <div className={classes.verifyCodeContainer}>
-        <input
-          className={classes.verifyCodeInput}
-          onKeyDown={handleKeyDown}
-          maxLength="4"
-          pattern="\d*"
-          onChange={props.onChange}
-          value={props.value}
-          autoFocus
-        />
+    <FormControl classes={{ root: classes.formControlRoot }} error={error}>
+      <div className={classes.verifyCodeWrapper}>
+        <div className={classes.verifyCodeContainer}>
+          <input
+            className={classes.verifyCodeInput}
+            onKeyDown={handleKeyDown}
+            maxLength="4"
+            pattern="\d*"
+            onChange={onChange}
+            value={value}
+            autoFocus
+          />
+        </div>
       </div>
-      {props.error && <p className={classes.helperText}>{props.helperText}</p>}
-    </div>
+      {helperText && (
+        <FormHelperText classes={{ root: classes.helperTextRoot }}>
+          {helperText}
+        </FormHelperText>
+      )}
+    </FormControl>
   );
 });
 
--- src/components/VerifyCode/stories.js
+++ src/components/VerifyCode/stories.js
@@ -7,16 +7,25 @@ import { muiTheme } from 'storybook-addon-material-ui';
 // Import our component from this folder
 import VerifyCode from './VerifyCode';
 
-import theme from '../../style/theme';
+import theme from '../../style/themeHalva';
 
 storiesOf('VerifyCode', module)
   .addDecorator(muiTheme([theme]))
-  .add('Default', () => <VerifyCode onChange={action('onChange')} />)
+  .add('Default', () => (
+    <VerifyCode
+      onChange={action('onChange')}
+      helperText={
+        <>
+          Отправить код повторно через <strong>25 сек</strong>
+        </>
+      }
+    />
+  ))
   .add('Error', () => (
     <VerifyCode
       onChange={action('onChange')}
       error={true}
       helperText={'Неверный код'}
-      classes={{ helperText: '11111' }}
+      classes={{ helperTextRoot: '11111' }}
     />
   ));
