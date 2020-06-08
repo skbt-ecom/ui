df41e762d9746d5e9529235df86528a7c74ce1b4
Tahir Nurmiev <nurmievtk@sovcombank.ru>
2020-06-08 19:46:28 +0300

    remove fixed width & extra div

--- src/components/FormLanding/fields/SubmitButton/index.js
+++ src/components/FormLanding/fields/SubmitButton/index.js
@@ -2,25 +2,21 @@ import React, { useContext } from 'react';
 import MaterialButton from '../../../Button';
 import { FormContext } from '../../FormContext/Form';
 
-import useStyles from './styles';
-
 const Button = ({ classsesComponent, ...props }) => {
   const context = useContext(FormContext);
-  const classesWrapper = useStyles(props);
+
   const onClick = () => {
     context.onSubmit();
   };
 
   return (
-    <div className={classesWrapper.container}>
-      <MaterialButton
-        color={'primary'}
-        fullWidth
-        {...props}
-        classes={classsesComponent}
-        onClick={onClick}
-      />
-    </div>
+    <MaterialButton
+      color={'primary'}
+      fullWidth
+      {...props}
+      classes={classsesComponent}
+      onClick={onClick}
+    />
   );
 };
 
