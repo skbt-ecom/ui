0505986e1836216a56f614822cf34482826e9580
Tahir Nurmiev <nurmievtk@sovcombank.ru>
2020-12-11 20:24:21 +0300

    feat(header): add Halva header

--- src/components/Header/header.stories.js
+++ src/components/Header/header.stories.js
@@ -1,21 +1,28 @@
 import React from "react"
-// Import the storybook libraries
-import { storiesOf } from "@storybook/react"
-import { muiTheme } from "storybook-addon-material-ui"
-import { action } from "@storybook/addon-actions"
 
-// Import our component from this folder
-import Header from "./Header"
+import { muiTheme } from "storybook-addon-material-ui"
+
+import HeaderHalva from "./Halva"
 
 import theme from "../../style/theme"
 
-storiesOf("Header", module)
-  .addDecorator(muiTheme([theme]))
-  .add("Default", () => <Header withButton onButtonClick={action("onButtonClick")} />)
-  .add("withHalvaLogo", () => <Header type={"withHalvaLogo"} />)
-  .add("withButtonProps", () => (
-    <Header
-      buttonProps={{ color: "secondary", label: "Присоединиться" }}
-      onButtonClick={action("onButtonClick")}
-    />
-  ))
+export default {
+  title: "Header",
+  decorators: [muiTheme([theme])],
+}
+
+export function Halva() {
+  return (
+    <div style={{ border: "1px solid #000" }}>
+      <HeaderHalva classes={{ logo: "asdasd", other: { header: "asd" } }} />
+    </div>
+  )
+}
+
+export function withRightBlock() {
+  return (
+    <div style={{ border: "1px solid #000" }}>
+      <HeaderHalva classes={{ logo: "asdasd", other: { header: "asd" } }}>right</HeaderHalva>
+    </div>
+  )
+}
