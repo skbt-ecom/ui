73faa644f756e03fad80ec0ea9f73182e68945bb
Tahir Nurmiev <nurmievtk@sovcombank.ru>
2021-04-30 22:18:02 +0300

    fix: canUseDom relations

--- src/utils/getSearchParam.js
+++ src/utils/getSearchParam.js
@@ -1,7 +1,7 @@
 import canUseDom from "./canUseDom"
 
 export default function getSearchParam(key) {
-  const search = canUseDom ? "" : window.location.search
+  const search = canUseDom ? window.location.search : ""
   const searchParams = new URLSearchParams(search)
   const value = searchParams.get(key)
 
