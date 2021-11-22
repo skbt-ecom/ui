278223c468b7ee2453a6cdb9d489b11d9ab6ae62
Tahir Nurmiev <nurmievtk@sovcombank.ru>
2021-11-22 14:39:17 +0300

    fix: temporarily disable form datalayer
    n

--- src/components/FormLanding/FormLanding.js
+++ src/components/FormLanding/FormLanding.js
@@ -1,5 +1,5 @@
-import React, { useEffect } from "react"
-import { pushToDataLayerLoadedStep } from "../../utils/pushToDataLayerUtils"
+import React from "react"
+// import { pushToDataLayerLoadedStep } from "../../utils/pushToDataLayerUtils"
 
 import { Form } from "./FormContext/Form"
 
@@ -9,9 +9,9 @@ const FormLanding = (props, ref) => {
   const classes = useStyles(props)
   const { onSubmit, onChangeFields, children, step } = props
 
-  useEffect(() => {
-    if (step) pushToDataLayerLoadedStep(step)
-  }, [step])
+  // useEffect(() => {
+  //   if (step) pushToDataLayerLoadedStep(step)
+  // }, [step])
 
   return (
     <div className={classes.container}>
