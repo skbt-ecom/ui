ee5792aa638f7b95b675e49ad862f5872a8432b0
usmanovrr1 <usmanovrr1@sovcombank.ru>
2022-03-21 10:07:01 +0300

    fix: fix +78
    fix +78

--- src/components/FormLanding/validators/phoneValidator.js
+++ src/components/FormLanding/validators/phoneValidator.js
@@ -2,13 +2,16 @@
  *
  * @param {string} phone ex. 71111111111
  */
-export const phoneValidator = phone => {
+export const phoneValidator = (phone) => {
+  if (phone[1] === "8") {
+    return "Введите корректный номер телефона"
+  }
   if (phone.length !== 11) {
     return "Введите номер телефона полностью"
   }
-  const re = /^(8|7)(3|4|5|6|8|9)/
+  const re = /^(8|7)(3|4|5|6|9)/
   if (!re.test(phone)) {
-    return "Код города/оператора должен начинаться с цифры 3, 4, 5, 6, 8, 9"
+    return "Код города/оператора должен начинаться с цифры 3, 4, 5, 6, 9"
   }
 
   return null
