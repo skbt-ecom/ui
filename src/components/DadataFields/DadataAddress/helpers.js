8db0f550c117457445eecc3449b4d12cd5edba9c
usmanovrr1 <usmanovrr1@sovcombank.ru>
2022-05-02 23:33:24 +0300

    fix: 🐛 add url and broker prop

--- src/components/DadataFields/DadataAddress/helpers.js
+++ src/components/DadataFields/DadataAddress/helpers.js
@@ -1,8 +1,14 @@
 import { getDadata } from "../getDadata"
 
-export function specifySuggestion(dadataValue) {
-  return getDadata("address", dadataValue.unrestricted_value, {
-    count: 1,
-    restrict_value: true,
-  }).then((getDadataResult) => getDadataResult?.suggestions?.[0])
+export function specifySuggestion(dadataValue, url, isBroker) {
+  return getDadata(
+    "address",
+    dadataValue.unrestricted_value,
+    {
+      count: 1,
+      restrict_value: true,
+    },
+    url,
+    isBroker
+  ).then((getDadataResult) => getDadataResult?.suggestions?.[0])
 }
