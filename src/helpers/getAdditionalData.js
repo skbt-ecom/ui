60fad164e074f5ac2a9b869300a68bba80bfa956
Gibadullin Ilyas <gibaullinim@sovcombank.ru>
2022-11-15 14:59:09 +0300

    feat: 🎸 getAdditionalData
    Add new value for AdditionalData - kameleoonData

--- src/helpers/getAdditionalData.js
+++ src/helpers/getAdditionalData.js
@@ -1,9 +1,19 @@
 import getSearchParams from "../utils/getSearchParams"
 
 export default function getAdditionalData() {
+  let kameleoonData
+
+  try {
+    const kameleoonVisitorCode = JSON.parse(localStorage.getItem('kameleoonVisitorCode'))
+    kameleoonData = {kameleoonVisitorCode}
+  } catch (error) {
+    console.error('kameleoonVisitorCode parsing error', error.message)
+  }
+
   return {
     url: window.location.href,
     cookie: window.document.cookie,
     qsParams: getSearchParams(),
+    kameleoonData
   }
 }
