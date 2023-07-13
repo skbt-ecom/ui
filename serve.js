1a1727d51ddb4103669d7dfc68ed72e846a115b5
Bogdan Xoxl <khohlovba@sovcombank.ru>
2023-07-13 21:05:19 +0300

    refactor(serve): revert serve.js

--- serve.js
+++ serve.js
@@ -1,23 +1,6 @@
 const express = require("express");
 const path = require("path");
 
-const fs = require("fs");
-
-//joining path of directory
-const directoryPath = path.join(__dirname, ".");
-//passsing directoryPath and callback function
-fs.readdir(directoryPath, function (err, files) {
-  //handling error
-  if (err) {
-    return console.log("Unable to scan directory: " + err);
-  }
-  //listing all files using forEach
-  files.forEach(function (file) {
-    // Do whatever you want to do with the file
-    console.log(file);
-  });
-});
-
 // Constants
 const PORT = 8080;
 const HOST = "0.0.0.0";
