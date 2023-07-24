31334c1534f7993f64cbbdf4401a7b3e3611f44f
Bogdan Xoxl <khohlovba@sovcombank.ru>
2023-07-24 09:23:47 +0300

    fix(serve): remove some code

--- serve.js
+++ serve.js
@@ -10,14 +10,6 @@ const app = express();
 app.use(express.static("storybook-static"));
 app.use(express.static("playroom-static"));
 
-app.get("/playroom", (req, res) => {
-  res.sendFile(path.join("playroom-static", "index.html"), { root: __dirname });
-});
-
-app.get("/", (req, res) => {
-  res.sendFile(path.join("storybook-static", "index.html"), { root: __dirname });
-});
-
 app.listen(PORT, HOST);
 
 console.log(`Running on http://${HOST}:${PORT}`);
