76a7ea6218b8ee5d257b30232165404b55458b12
Danis <minnigalindr@sovcombank.ru>
2024-01-30 09:03:43 +0300

    feat(fixed pipline): fixed pipline

--- src/components/calculator/Slider/Slider.test.tsx
+++ src/components/calculator/Slider/Slider.test.tsx
@@ -3,15 +3,21 @@ import Slider from "@mui/material/Slider";
 
 import type { ITestSlider } from "./types";
 
-// Mocking getBoundingClientRect
-jest.spyOn(HTMLElement.prototype, "getBoundingClientRect").mockImplementation(
-  () =>
-    ({
-      left: 19.572917938232422,
-      width: 563.5208740234375,
-      toJSON: () => {},
-    } as ITestSlider)
-);
+const mockedBoundingClientRect: ITestSlider = {
+  left: 19.572917938232422,
+  width: 563.5208740234375,
+  toJSON: () => {},
+  height: 0,
+  x: 0,
+  y: 0,
+  bottom: 0,
+  right: 0,
+  top: 0,
+};
+
+jest
+  .spyOn(HTMLElement.prototype, "getBoundingClientRect")
+  .mockImplementation(() => mockedBoundingClientRect);
 
 describe("slider Component", () => {
   it("updates value when slider thumb is moved", () => {
